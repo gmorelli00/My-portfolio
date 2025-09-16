b67300857a30907938b582be1aeab16e5a22bcb5
gmorelli00 <morelligiovannimg@gmail.com>
2025-09-16 18:50:02 +0200

    alpha 0.2

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -7,7 +7,6 @@ function Footer() {
         <h1 id="contact" className="text-5xl font-bold">Contact</h1>
         <div className="flex flex-col justify-center items-center mt-20 w-full space-y-1">
             <p className="text-lg">Email: <a href="mailto:morelligiovannimg@gmail.com" className="text-blue-500 hover:underline">morelligiovannimg@gmail.com</a></p>
-            <p className="text-lg">Phone: <a href="tel:+393921205875" className="text-blue-500 hover:underline">+39 392 120 5875</a></p>
             <p className="text-lg">LinkedIn: <a href="https://www.linkedin.com/in/giovanni-morelli-272a1a330/" className="text-blue-500 hover:underline">linkedin.com/in/giovanni-morelli-272a1a330/</a></p>
             <p className="text-lg">GitHub: <a href="https://github.com/gmorelli00" className="text-blue-500 hover:underline">github.com/gmorelli00</a></p>
         </div>
