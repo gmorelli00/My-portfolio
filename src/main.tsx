fbfa3bbf6310a6950323e9e264544df9022a1bb7
gmorelli00 <morelligiovannimg@gmail.com>
2026-04-03 16:03:09 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/My-portfolio/">
       <App />
     </BrowserRouter>
   </StrictMode>,
